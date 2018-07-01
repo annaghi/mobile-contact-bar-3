5cc5d661395e0016cd0b6b64f0f1259561bb8861
Anna Bansaghi <anna.bansaghi@gmail.com>
2018-07-01 18:37:32 +0200

    Set up PHP CS with WP CS, refactored PHP files

--- uninstall.php
+++ uninstall.php
@@ -1,49 +1,50 @@
 <?php
-
-defined( 'ABSPATH' ) and defined( 'WP_UNINSTALL_PLUGIN' ) or exit;
-
-
 /**
- * Database cleaning process
+ * Mobile Contact Bar Uninstall
  *
- * @since 0.1.0
+ * Uninstalling Mobile Contact Bar deletes options.
  *
+ * @package Mobile_Contact_Bar\Uninstaller
  * @global $wpdb
  */
 
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
+
+
 global $wpdb;
 
-if( is_multisite() )
-{
-    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-    foreach( $blog_ids as $blog_id )
-    {
-        switch_to_blog( $blog_id );
 
-        $plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'mobile_contact_bar%'" );
-        foreach( $plugin_options as $option )
-        {
-            delete_option( $option->option_name );
-        }
+// Clear Options table.
+if ( is_multisite() ) {
+	$mobile_contact_bar_blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+	foreach ( $mobile_contact_bar_blog_ids as $blog_id ) {
+		switch_to_blog( $blog_id );
 
-        restore_current_blog();
-    }
-}
-else
-{
-    $plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'mobile_contact_bar%'" );
-    foreach( $plugin_options as $option )
-    {
-        delete_option( $option->option_name );
-    }
+		$mobile_contact_bar_plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'mobile_contact_bar%'" );
+		foreach ( $mobile_contact_bar_plugin_options as $option ) {
+			delete_option( $option->option_name );
+		}
+
+		restore_current_blog();
+	}
+} else {
+	$mobile_contact_bar_plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'mobile_contact_bar%'" );
+	foreach ( $mobile_contact_bar_plugin_options as $option ) {
+		delete_option( $option->option_name );
+	}
 }
 
-if( ! $user = wp_get_current_user() )
-{
-    wp_die( -1 );
+
+// Clear Usermeta table.
+$mobile_contact_bar_user = wp_get_current_user();
+if ( ! $mobile_contact_bar_user ) {
+	wp_die( -1 );
 }
-$user_options = $wpdb->get_results( "SELECT meta_key FROM $wpdb->usermeta WHERE meta_key LIKE '%mobile-contact-bar%'" );
-foreach( $user_options as $option )
-{
-    delete_user_option( $user->ID, $option->meta_key, true );
+$mobile_contact_bar_user_options = $wpdb->get_results( "SELECT meta_key FROM $wpdb->usermeta WHERE meta_key LIKE '%mobile-contact-bar%'" );
+foreach ( $mobile_contact_bar_user_options as $option ) {
+	delete_user_option( $mobile_contact_bar_user->ID, $option->meta_key, true );
 }
+
+
+// Clear any cached data that has been removed.
+wp_cache_flush();
