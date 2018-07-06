f75d3c04f5e49cd0c6aa4e80462ecf3dac1cd8de
Anna Bansaghi <anna.bansaghi@gmail.com>
2018-07-06 10:24:43 +0200

    Apply JS Standard Code Style

--- assets/js/public/scripts.js
+++ assets/js/public/scripts.js
@@ -1,69 +1,63 @@
 /*!
- * Mobile Contact Bar 2.0.1 by Anna Bansaghi - http://mobilecontactbar.com
+ * Mobile Contact Bar 2.1.0 by Anna Bansaghi - http://mobilecontactbar.com
  * License GPL-3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
+(function (document) {
+  'use strict'
 
+  var MobileContactBar = {
 
-(function( document ) {
-    'use strict';
+    createCookie: function (name, value, days) {
+      var expires, date
 
-    var MobileContactBar = {
+      if (days) {
+        date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = '; expires=' + date.toGMTString()
+      } else {
+        expires = ''
+      }
+      document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + expires + '; path=/'
+    },
 
-        createCookie: function( name, value, days ) {
-            var expires, date;
+    readCookie: function (name) {
+      var nameEQ, ca, c
 
-            if( days ) {
-                date = new Date();
-                date.setTime( date.getTime() + ( days * 24 * 60 * 60 * 1000 ));
-                expires = '; expires=' + date.toGMTString();
+      nameEQ = encodeURIComponent(name) + '='
+      ca = document.cookie.split(';')
 
-            } else {
-                expires = '';
-            }
-            document.cookie = encodeURIComponent( name ) + '=' + encodeURIComponent( value ) + expires + '; path=/';
-        },
-
-        readCookie: function( name ) {
-            var nameEQ, ca, c;
-
-            nameEQ = encodeURIComponent( name ) + '=';
-            ca = document.cookie.split( ';' );
-
-            for( var i = 0; i < ca.length; i++ ) {
-                c = ca[i];
-                while( c.charAt(0) === ' ' ) {
-                    c = c.substring( 1, c.length );
-                }
-                if( c.indexOf( nameEQ ) === 0 ) {
-                    return decodeURIComponent( c.substring( nameEQ.length, c.length ));
-                }
-            }
-            return null;
-        },
-
-        eraseCookie: function( name ) {
-            MobileContactBar.createCookie( name, '', -1 );
+      for (var i = 0; i < ca.length; i++) {
+        c = ca[i]
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1, c.length)
         }
-    };
-
-    var toggle = document.getElementById('mobile-contact-bar-toggle-checkbox');
-    if( null !== toggle ) {
-        toggle.addEventListener('click', function( event ) {
-            event.stopPropagation();
-
-            if( event.target.checked ) {
-                MobileContactBar.createCookie( 'mobile_contact_bar_toggle', 'closed' );
-            } else {
-                MobileContactBar.createCookie( 'mobile_contact_bar_toggle', 'open' );
-            }
-        });
-
-
-        if( 'closed' == MobileContactBar.readCookie( 'mobile_contact_bar_toggle' )) {
-            toggle.click();
+        if (c.indexOf(nameEQ) === 0) {
+          return decodeURIComponent(c.substring(nameEQ.length, c.length))
         }
+      }
+      return null
+    },
+
+    eraseCookie: function (name) {
+      MobileContactBar.createCookie(name, '', -1)
     }
+  }
 
+  var toggle = document.getElementById('mobile-contact-bar-toggle-checkbox')
+  if (toggle !== null) {
+    toggle.addEventListener('click', function (event) {
+      event.stopPropagation()
 
-})( document );
+      if (event.target.checked) {
+        MobileContactBar.createCookie('mobile_contact_bar_toggle', 'closed')
+      } else {
+        MobileContactBar.createCookie('mobile_contact_bar_toggle', 'open')
+      }
+    })
+
+    if (MobileContactBar.readCookie('mobile_contact_bar_toggle') === 'closed') {
+      toggle.click()
+    }
+  }
+})(document)
