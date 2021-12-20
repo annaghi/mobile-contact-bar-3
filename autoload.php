d385f0a63dfffcaf4f37c054716fcb62d4e0017a
Anna Bansaghi <anna.bansaghi@gmail.com>
2021-12-20 16:53:52 +0100

    Update autoload

--- autoload.php
+++ autoload.php
@@ -8,12 +8,12 @@ spl_autoload_register( function ( $class_name )
     $namespaces = ['MobileContactBar\\' => __DIR__ . '/php/'];
     foreach ( $namespaces as $prefix => $base_dir )
     {
-        $len = strlen( $prefix );
-        if ( 0 !== strncmp( $prefix, $class_name, $len ))
+        $length = strlen( $prefix );
+        if ( 0 !== strncmp( $prefix, $class_name, $length ))
         {
             continue;
         }
-        $file = $base_dir . str_replace( '\\', '/', substr( $class_name, $len )) . '.php';
+        $file = $base_dir . str_replace( '\\', '/', substr( $class_name, $length )) . '.php';
         if ( ! file_exists( $file ))
         {
             continue;
