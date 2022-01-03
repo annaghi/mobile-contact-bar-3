bd4338a65b730cde7f84bc45a6fc29d7e94aad7a
Anna Bansaghi <anna.bansaghi@gmail.com>
2022-01-03 13:19:30 +0100

    Update uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,15 +3,16 @@
 defined( 'ABSPATH' ) and defined( 'WP_UNINSTALL_PLUGIN' ) || exit();
 
 
-global $wpdb;
-
 /**
  * Cleans the database from all plugin related data.
  * Clears plugin related cron events.
  *
  * @global $wpdb
  */
-function abmcb_uninstall() {
+function abmcb_uninstall()
+{
+    global $wpdb;
+
     $wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '%mobile_contact_bar%'" );
     $wpdb->query( "DELETE FROM {$wpdb->usermeta} WHERE meta_key LIKE '%mobile_contact_bar%'" );
     wp_clear_scheduled_hook( 'mobile_contact_bar_weekly_scheduled_events' );
